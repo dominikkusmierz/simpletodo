5585ff95e6ddb46616415b3015d01ef452810262
Dominik Kuśmierz <dominik.kusmierz99@gmail.com>
2023-03-10 19:31:30 +0100

    Added delete function, new render function

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ else {
   ];
 }
 function createItem(content) {
-  let id = list.length;
+  const id = new Date().getTime();
   let element = { id: id, content: content, isDone: false };
   list.push(element);
   saveItem();
@@ -22,17 +22,37 @@ function saveItem() {
   localStorage.setItem("list", JSON.stringify(list));
 }
 
+function removeItem(id) {
+  list=list.filter(function (item) {
+    if (item.id == id) {
+      return false;
+    } else {
+      return true;
+    }
+  });
+}
+
+function deleteItem(e) {
+  const deleteButton = e.target;
+  const idToDelete = deleteButton.id;
+  console.log(idToDelete);
+  removeItem(idToDelete);
+  saveItem();
+  loadItems(list);
+}
+
 const addButton = document.querySelector(".add-button");
 const addInput = document.querySelector(".add-item-input");
 const items = document.querySelector(".items");
-
 window.addEventListener("DOMContentLoaded", function () {
   loadItems(list);
+  const deleteButton = document.querySelector(".delete-button");
 });
 
 addButton.addEventListener("click", function () {
   addInput.classList.toggle("hide");
 });
+
 addInput.addEventListener("keypress", function (e) {
   let size = Object.keys(list).length;
   let inputvalue = addInput.value;
@@ -47,11 +67,32 @@ addInput.addEventListener("keypress", function (e) {
 });
 
 function loadItems(menu) {
-  let displayItems = menu.map(function (item) {
-    return ` <div class="item">
-                <input type="checkbox" name="" id=""> <p>${item.content}</p>
-            </div>`;
+  // let displayItems = menu.map(function (item) {
+  //   return ` <div class="item">
+  //               <input type="checkbox" name="" id=""> <p>${item.content}</p>
+  //           </div>`;
+  // });
+
+  //
+  const itembox = document.querySelector(".items");
+  itembox.innerHTML = "";
+
+  menu.forEach(function (menu) {
+    const item = document.createElement("div");
+    item.classList = "item";
+    item.innerText = menu.content;
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.classList = "item-checkbox";
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList = "delete-button";
+    deleteButton.onclick = deleteItem;
+    deleteButton.classList = "delete-button fa-regular fa-trash-can fa-xs";
+    deleteButton.id = menu.id;
+    item.appendChild(deleteButton);
+    items.appendChild(item);
+    item.prepend(checkbox);
   });
-  displayItems = displayItems.join("");
-  items.innerHTML = displayItems;
 }
