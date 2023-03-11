a00b0dcd3ff79839e3cec84a1501a4479a53780d
Dominik Kuśmierz <dominik.kusmierz99@gmail.com>
2023-03-11 13:25:41 +0100

    Mobile view, visual changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let list;
 const savedList = JSON.parse(localStorage.getItem("list"));
-
+//model//////////////
 if (Array.isArray(savedList)) list = savedList;
 else {
   list = [
@@ -12,22 +12,24 @@ else {
     },
   ];
 }
-function getDate(){
-  const date=new Date();
+function getDate() {
+  const date = new Date();
   const day = date.getDay();
-  const month = date.getMonth();
+  const month = date.getMonth() + 1;
   const year = date.getFullYear();
   const myDate = year + "-" + month + "-" + day;
   return myDate;
 }
 function createItem(content) {
-  const date = new Date();
-  const id = date.getTime();
-  let myDate=getDate();
-  console.log(myDate);
-  let element = { id: id, content: content, isDone: false, date: myDate };
-  list.push(element);
-  saveItem();
+  if (content == "") return;
+  else {
+    const date = new Date();
+    const id = date.getTime();
+    let myDate = getDate();
+    let element = { id: id, content: content, isDone: false, date: myDate };
+    list.push(element);
+    saveItem();
+  }
 }
 
 function saveItem() {
@@ -44,6 +46,7 @@ function removeItem(id) {
   });
 }
 
+//controller/////////////
 function deleteItem(e) {
   const deleteButton = e.target;
   const idToDelete = deleteButton.id;
@@ -52,7 +55,7 @@ function deleteItem(e) {
   saveItem();
   render();
 }
-
+//selecting my buttons
 const addButton = document.querySelector(".add-button");
 const addInput = document.querySelector(".add-item-input");
 const items = document.querySelector(".items");
@@ -60,55 +63,54 @@ window.addEventListener("DOMContentLoaded", function () {
   render();
   const deleteButton = document.querySelector(".delete-button");
 });
+//button + click event
 
-addButton.addEventListener("click", function () {
-  addInput.classList.toggle("hide");
-  addInput.focus();
-});
+// addButton.addEventListener("click", function () {
+//   addInput.classList.toggle("hide");
+//   addInput.focus();
+// });
 
+//Input item event
 addInput.addEventListener("keypress", function (e) {
   let size = Object.keys(list).length;
   let inputvalue = addInput.value;
   if (e.key === "Enter") {
     createItem(inputvalue);
 
-    addInput.classList.toggle("hide");
+    // addInput.classList.toggle("hide");
     addInput.value = "";
   }
 
- render();
+  render();
 });
-
+//view//////////////////////
+//Items made in this day
 function currentItems(menu) {
- 
   const itembox = document.querySelector(".items");
   itembox.innerHTML = "";
 
   menu.forEach(function (menu) {
-    if(getDate()==menu.date){
-    const item = document.createElement("div");
-    item.classList = "item";
-    item.innerText = menu.content;
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.classList = "item-checkbox";
+    if (getDate() == menu.date) {
+      const item = document.createElement("div");
+      item.classList = "item";
+      item.innerText = menu.content;
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.classList = "item-checkbox";
 
-    const deleteButton = document.createElement("button");
-    deleteButton.classList = "delete-button";
-    deleteButton.onclick = deleteItem;
-    deleteButton.classList = "delete-button fa-regular fa-trash-can fa-xs";
-    deleteButton.id = menu.id;
-    item.appendChild(deleteButton);
-    items.appendChild(item);
-    item.prepend(checkbox);
+      const deleteButton = document.createElement("button");
+      deleteButton.classList = "delete-button";
+      deleteButton.onclick = deleteItem;
+      deleteButton.classList = "delete-button fa-regular fa-trash-can fa-xs";
+      deleteButton.id = menu.id;
+      item.appendChild(deleteButton);
+      items.appendChild(item);
+      item.prepend(checkbox);
     }
   });
 }
 
-
-function render(){
-  
-currentItems(list);
+//rendering all items
+function render() {
+  currentItems(list);
 }
-
-
